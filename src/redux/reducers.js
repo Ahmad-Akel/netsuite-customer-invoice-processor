376125cc0fd4ab26c876bce0ae5bb0a65b6076d8
ahmad <ahmad.oudai1999@gmail.com>
2025-01-20 22:55:46 +0100

    feat: update state managment

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,21 +1,35 @@
 const initialState = {
-  customers: [],
-  selectedCustomers: [],
   invoices: [],
+  selectedCustomers: [],
+  customers: [],
+  loadingInvoices: false,
+  errorInvoices: null,
+  loadingCustomers: false,
+  errorCustomers: null,
 };
 
-const rootReducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_CUSTOMERS":
-      return { ...state, customers: action.payload };
-    case "SET_SELECTED_CUSTOMERS":
-      return { ...state, selectedCustomers: action.payload };
-    case "SET_INVOICES":
-      return { ...state, invoices: action.payload };
-    case "FETCH_CUSTOMERS_SUCCESS":
-      return { ...state, customers: action.payload };
+    case "FETCH_INVOICES_REQUEST":
+      return { ...state, loadingInvoices: true, errorInvoices: null };
     case "FETCH_INVOICES_SUCCESS":
-      return { ...state, invoices: action.payload };
+      return { ...state, invoices: action.payload, loadingInvoices: false };
+    case "FETCH_INVOICES_ERROR":
+      return {
+        ...state,
+        loadingInvoices: false,
+        errorInvoices: action.payload,
+      };
+    case "FETCH_CUSTOMERS_REQUEST":
+      return { ...state, loadingCustomers: true, errorCustomers: null };
+    case "FETCH_CUSTOMERS_SUCCESS":
+      return { ...state, customers: action.payload, loadingCustomers: false };
+    case "FETCH_CUSTOMERS_ERROR":
+      return {
+        ...state,
+        loadingCustomers: false,
+        errorCustomers: action.payload,
+      };
     case "SELECT_CUSTOMER":
       return { ...state, selectedCustomers: action.payload };
     default:
@@ -23,4 +37,4 @@ const rootReducer = (state = initialState, action) => {
   }
 };
 
-export default rootReducer;
+export default reducer;
